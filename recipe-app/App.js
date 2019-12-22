3ac1444403dfc9010c3fa6261db673094c500338
adam23stevens <adam23stevens@gmail.com>
2019-12-22 21:10:17 +0000

    baubles clue

--- recipe-app/App.js
+++ recipe-app/App.js
@@ -11,13 +11,13 @@ DeleteClues();
 
 InsertClue('Clue 1', 'Let\'s start this off simple. This is easier than most. You\'ll not always be so jammy, spreading it on toast', 2, 'sprouts', 1);
 InsertClue('Clue 2', 'Well done for the first one. Next is a game in itself. Where else would you find all the villagers on the shelf', 3, 'stocking', 0);
-InsertClue('Clue 3', 'Doing so well now. You fill me with pride. Wait, what\'s that sound? Has my red nose just arrived?!', 4, 'snowman', 0);
+InsertClue('Clue 3', 'Doing so well now. You fill me with pride. Wait, what\'s that sound? Has Rudolph arrived?!', 4, 'snowman', 0);
 InsertClue('Clue 4', 'While you\'re still outisde, you might want to wonder, the next clue just may be, for a registration number', 5, 'Candy Cane', 0);
 InsertClue('Clue 5', 'Golly gosh I best make this harder! You make it look easy to do. Now I sure do fancy some choklit. 25 of them will do!', 6, 'Gold rings', 0);
 InsertClue('Clue 6', 'Binary is good fun eh, now you\'re all in for a treat. Just ppppick up the next one under some happy feet.', 7, 'pantomime', 0);
 InsertClue('Clue 7', 'Wood is made from trees. You ought to know so you should. But have you ever seen a tree made out of wood?', 8, 'jingle bells', 0);
-InsertClue('Clue 8', 'This next one is written on a door that\'s quite large. To give you a further clue, it rhymes with Nigel Farage', 9, 'nutmeg', 0);
-InsertClue('Clue 9', 'You\'ve been running and searching. You\'ve been all round the block. To find this next one, look in the big sock', 10, 'santa baby', 0);
+InsertClue('Clue 8', 'This next one is found behind a door that\'s quite large. To give a clue further, it rhymes with Nigel Farage', 9, 'nutmeg', 0);
+InsertClue('Clue 9', 'You\'ve been running and searching. You\'ve been all round the blocks. To find this next one, look in the big socks', 10, 'santa baby', 0);
  InsertClue('Final Clue', 'Now bounce up and spring, for all you need to win, is dial me up and ring, AND GIVE A GOOD CHRISTMASSY SING!', 99, '', 0);
 
 
